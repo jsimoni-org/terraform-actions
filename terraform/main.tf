8ec3ff15662c99a1d8ad8da4637fbed645c82119
Craig Kilpatrick <ckilpatrick@ipipeline.com>
2020-05-01 14:36:24 -0700

    Fixing formatting issue

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,14 +1,14 @@
 provider "aws" {
-  region     = "us-east-1"
+  region = "us-east-1"
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-0323c3dd2da7fb37d"
+  ami = "ami-0323c3dd2da7fb37d"
 
   tags = {
-    Name = "Test Instance"
-    Creator = "ckilpatrick@ipipeline.com"
+    Name        = "Test Instance"
+    Creator     = "ckilpatrick@ipipeline.com"
     Environment = "Sandbox"
-    Terraform = true
+    Terraform   = true
   }
 }
