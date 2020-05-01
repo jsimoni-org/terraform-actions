1a175ce38acf436992e7e2f6942bb5eede1ae44c
Craig Kilpatrick <34137636+ckilpatrick20@users.noreply.github.com>
2020-05-01 15:28:20 -0700

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,6 +10,7 @@ resource "aws_instance" "example" {
     Name        = "Test Instance"
     Creator     = "ckilpatrick@ipipeline.com"
     Environment = "Sandbox"
+    Product     = "baby_yoda"
     Terraform   = true
   }
 }
