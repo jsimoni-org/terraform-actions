99e48004a571c7e0b72f6c011604b8688a5a601f
Craig Kilpatrick <ckilpatrick@ipipeline.com>
2020-05-11 10:45:31 -0700

    Testing

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,11 +4,11 @@ provider "aws" {
 
 resource "aws_instance" "example" {
   ami           = "ami-0323c3dd2da7fb37d"
-  instance_type = "t2.micro"
+  # instance_type = "t2.micro"
 
-  root_block_device {
-    encrypted = true
-  }
+  # root_block_device {
+  #   encrypted = true
+  # }
 
   tags = {
     Name        = "Test Instance"
