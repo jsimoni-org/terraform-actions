9fb721cbd4bcff4c1da95745394129f2f9990a7b
Craig Kilpatrick <ckilpatrick@ipipeline.com>
2020-05-20 07:22:46 -0700

    testing

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ resource "aws_instance" "example" {
     Name        = "Test Instance"
     Creator     = "ckilpatrick@ipipeline.com"
     Environment = "Sandbox"
-    Product     = "ccoe"
+    Product     = "ipipeline"
     Terraform   = true
   }
 }
