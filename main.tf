a5a863baa36db0a25dfddce10183958d73f6cb67
Craig Kilpatrick <ckilpatrick@ipipeline.com>
2020-08-14 09:38:24 -0700

    feat: changing instance type to t2

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 resource "aws_instance" "example" {
   ami           = "ami-0323c3dd2da7fb37d"
-  instance_type = "t3.micro"
+  instance_type = "t2.micro"
 
   root_block_device {
     encrypted = true
@@ -22,7 +22,7 @@ resource "aws_instance" "example" {
 
 resource "aws_instance" "example_2" {
   ami           = "ami-0323c3dd2da7fb37d"
-  instance_type = "t3.micro"
+  instance_type = "t2.micro"
 
   root_block_device {
     encrypted = true
